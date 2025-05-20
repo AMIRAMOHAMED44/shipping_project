b5ff843e80326e2f4c1909f1918519ebdb236322
omaralaa15 <oak1512001@gmail.com>
2025-05-20 20:47:58 +0300

    Agent & auth & refresh token

--- frontend/shipping/src/App.jsx
+++ frontend/shipping/src/App.jsx
@@ -1,16 +1,20 @@
 import Home from "./components/home/Home";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Header/Header";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateShipment from "./components/shipments/CreateShipment";
 import ShipmentList from "./components/shipments/ShipmentList";
+import AvailableShipments from "./components/availableShipments/AvailableShipments";
+import MyShipments from "./components/myShipments/MyShipments";
+import MyEarnings from "./components/myEarnings/MyEarnigns";
+import { AuthProvider } from "./context/AuthContext";
 function App() {
-
   return (
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
         <div className="flex flex-col min-h-screen">
           <Navbar />
           <main className="flex-grow">
@@ -20,11 +24,18 @@ function App() {
               <Route path="/register" element={<Register />} />
               <Route path="/shipments/create" element={<CreateShipment />} />
               <Route path="/shipments/list" element={<ShipmentList />} />
+              <Route
+                path="/shipments/available"
+                element={<AvailableShipments />}
+              />
+              <Route path="/shipments/my-shipments" element={<MyShipments />} />
+              <Route path="my-earnings" element={<MyEarnings />} />
             </Routes>
           </main>
           <Footer />
         </div>
-      </BrowserRouter>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
